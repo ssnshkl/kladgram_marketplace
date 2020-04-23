4621855bd44b86069346cb4f1be19e3520a76670
ssnshkl <46741073+ssnshkl@users.noreply.github.com>
2020-04-23 16:54:25 +0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import Vuetify from 'vuetify'
+import BuyModalComponent from '@/components/Shared/BuyModal'
+import * as fb from 'firebase'
+import 'vuetify/dist/vuetify.min.css'
+import './stylus/main.styl'
+// import colors from 'vuetify/es5/util/colors'
+
+Vue.use(Vuetify, {
+  // theme: {
+  //   primary: "#f44336",
+  //   secondary: "#9575CD",
+  //   accent: "#9c27b0",
+  //   error: "#f44336",
+  //   warning: "#3949AB",
+  //   info: "#2196f3",
+  //   success: "#4caf50"
+  // }
+})
+Vue.component('app-buy-modal', BuyModalComponent)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>',
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyBXIg0S-mW8rJVcuGOdaKLA2VCvRDum9g4',
+      authDomain: 'kladgram.firebaseapp.com',
+      databaseURL: 'https://kladgram.firebaseio.com',
+      projectId: 'kladgram',
+      storageBucket: 'kladgram.appspot.com',
+      messagingSenderId: '163947210017',
+      appId: '1:163947210017:web:053599462c443493610a60',
+      measurementId: 'G-QFWCLXTF40'
+    })
+
+    fb.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.$store.dispatch('autoLoginUser', user)
+      }
+    })
+
+    this.$store.dispatch('fetchAds')
+  }
+})
